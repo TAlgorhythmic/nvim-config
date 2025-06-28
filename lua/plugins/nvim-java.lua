52e5164ba512a612821c0bf799880d915ca133a6
Algorhythmics <intivioli@gmail.com>
2025-06-28 20:52:44 +0200

    Config update

--- lua/plugins/nvim-java.lua
+++ lua/plugins/nvim-java.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		local config = {
 			cmd = {os.getenv("HOME") .. "/.local/share/nvim/mason/bin/jdtls"},
-			root_dir = vim.fs.dirname(vim.fs.find({'gradlew', '.git', 'mvnw', 'pom.xml'}, { upward = true })[1]),
+			root_dir = vim.fs.dirname(vim.fs.find({'gradlew', 'mvnw', 'pom.xml'}, { upward = true })[1]),
 		}
 		require("jdtls").start_or_attach(config)
 		require("lspconfig").jdtls.setup({})
